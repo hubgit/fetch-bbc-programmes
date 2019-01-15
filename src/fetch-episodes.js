4a6ca786832d040abb4765f178294a43fa8292db
Alf Eaton <eaton.alf@gmail.com>
2019-01-15 12:33:00 +0000

    Use axios-collection

--- src/fetch-episodes.js
+++
@@ -1,5 +0,0 @@
-const { episodes, genres } = require('./api')
-
-genres().then(data => {
-  data.categories.filter(category => category.narrower).forEach(category => episodes(category.key))
-})
---
+++ src/fetch.js
@@ -0,0 +1,49 @@
+require('axios-debug-log')
+const fs = require('fs')
+const collection = require('axios-collection')
+
+// TODO: use axios retry and rate-limiting interceptors
+
+const buildURL = (path, page) => `http://clifton.api.bbci.co.uk/aps/${path}.json?page=${page || 1}`
+// `https://www.bbc.co.uk/${path}.json?page=${page || 1}`
+
+const episodes = async genre => {
+  const output = fs.createWriteStream(__dirname + `/../data/genres/${genre}.ndjson`)
+
+  const url = buildURL(`programmes/genres/${genre}/player/episodes`)
+
+  const items = collection(url, {
+    parse: response => response.data.episodes,
+    next: response => {
+      const { data: { offset, episodes, total, page } } = response
+
+      if (offset + episodes.length >= total) return null
+
+      return buildURL(`programmes/genres/${genre}/player/episodes`, page + 1)
+    }
+  })
+
+  for await (const item of items) {
+    output.write(JSON.stringify(item.programme) + '\n')
+  }
+}
+
+const fetch = async () => {
+  const url = buildURL('programmes/genres')
+
+  const items = collection(url, {
+    parse: response => response.data.categories,
+  })
+
+  for await (const item of items) {
+    if (item.narrower) {
+      await episodes(item.key)
+    }
+  }
+}
+
+fetch().then(() => {
+  console.log('Finished')
+}).catch(error => {
+  console.error(error)
+})
